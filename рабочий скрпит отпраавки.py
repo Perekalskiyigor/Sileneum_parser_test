687a80d26df2a15010c06b0f28a48a5c715b749a
Igor Perekalskiy <perekalskiy_igor@mail.ru>
2023-04-24 08:52:48 +0500

    рабочий

--- рабочий скрпит отпраавки.py
+++ рабочий скрпит отпраавки.py
@@ -1,13 +1,13 @@
 import smtplib
 import os
 from email.mime.text import MIMEText
+import openpyxl
 
-
-def send_email(message):
+def send_email(message,sender1):
     sender = "perekalskiy.igor@gmail.com"
-    password = "uwhwbcfrconlitnu"
+    password = "wnnshwomrmtxlsvo"
     # password = os.getenv("EMAIL_PASSWORD")
-    sender1 = 'perekalskiy_igor@mail.ru'
+    
 
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
@@ -25,9 +25,24 @@ def send_email(message):
         return f"{_ex}\nCheck your login or password please!"
 
 
+
 def main():
-    message = input("Type your message: ")
-    print(send_email(message=message))
+    # Чтение таблицы Excel и создание списка получателей и текста письма
+    wb = openpyxl.load_workbook('emails.xlsx')
+    ws = wb.active
+    send_to = []
+    body = []
+    for row in ws.iter_rows(min_row=2, values_only=True):
+         send_to.append(row[0])
+         body.append(row[1])
+         send_email(message=row[1], sender1 = row[0])
+         print(row[0])
+         print(row[1])
+         input()
+
+    #sender1 = 'perekalskiy_igor@mail.ru'
+    #message = input("Type your message: ")
+    #print(send_email(message=message, sender1 = sender1))
 
 
 if __name__ == "__main__":
